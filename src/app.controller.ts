a44e4f820822bc4268dae64b9a947ff3f690897f
wilsonleehk01 <150896810+wilsonleehk01@users.noreply.github.com>
2024-07-31 12:30:19 +0800

    feat: test-feat (#3)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// Controller is a decorator that defines a class as a controller in Nest.js
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
